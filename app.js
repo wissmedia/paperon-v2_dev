7bc65aa0ecddf1847599a9b0f87ee465d25fa92d
wissmedia <wissmedia@gmail.com>
2021-09-02 22:48:12 +0800

    Install and Use Morgan
    Install and Use Morgan as Logger

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // IMPORT DEPENDENCIES AND REQUIRED FILE
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 
 // LOAD ENV CONFIG
 dotenv.config({path: './config/config.env'})
@@ -8,6 +9,9 @@ dotenv.config({path: './config/config.env'})
 // CREATE EXPRESS APP 
 const app = express()
 const PORT = process.env.PORT || 2021
+
+// LOGGER
+app.use(morgan('dev'))
 
 // ROUTES (basic)
 app.get('/', (req, res) => {
