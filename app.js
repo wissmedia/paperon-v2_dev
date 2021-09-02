2c1d22f21a572906560e870a899c7e1ead1e158a
wissmedia <wissmedia@gmail.com>
2021-09-02 22:51:22 +0800

    Install and Use EJS
    Install and Use EJS as View Engine

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ dotenv.config({path: './config/config.env'})
 const app = express()
 const PORT = process.env.PORT || 2021
 
+// EJS
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
 // LOGGER
 app.use(morgan('dev'))
 
@@ -19,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // ROUTES (basic)
 app.get('/', (req, res) => {
-  res.send('its work')
+  res.render('index')
 })
 
 app.listen(PORT, () => { console.log(`Server Running on port ${PORT}`) })
