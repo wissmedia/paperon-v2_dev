d9351b35e97b8aada9396788fe3eec7a97959aaa
wissmedia <wissmedia@gmail.com>
2021-09-02 22:45:41 +0800

    Update app.js
    add dotenv and use ENV variable

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 // IMPORT DEPENDENCIES AND REQUIRED FILE
 const express = require('express')
+const dotenv = require('dotenv')
+
+// LOAD ENV CONFIG
+dotenv.config({path: './config/config.env'})
 
 // CREATE EXPRESS APP 
 const app = express()
+const PORT = process.env.PORT || 2021
 
 // ROUTES (basic)
 app.get('/', (req, res) => {
   res.send('its work')
 })
 
-app.listen(2021, () => { console.log('Server Running...') })
+app.listen(PORT, () => { console.log(`Server Running on port ${PORT}`) })
