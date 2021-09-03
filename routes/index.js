d4ac43a8ae8e7f5cac0cdb6facd3ee770365dcd4
wissmedia <wissmedia@gmail.com>
2021-09-03 16:03:01 +0800

    Create index.js

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
+
+// @desc    Login/Landing Page
+// @route   GET /
+router.get('/', ensureGuest, (req, res) => {
+  res.render('index')
+})
+
+// @desc    Dashboard
+// @route   GET /dashboard
+router.get('/dash', ensureAuth, async (req, res) => {
+
+  try {
+    // const stories = await Story.find({ user: req.user.id }).lean()
+    res.render('dashboard/index', {
+      user: req.user,
+      // stories
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+})
+
+module.exports = router
