5e85709defa533c397fab42fe229d6a59fff04e1
wissmedia <wissmedia@gmail.com>
2021-09-04 15:21:08 +0800

    Create Routes (blank)
    Public, qbank, quesioner, result

---
+++ routes/qbank.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const { ensureAuth } = require('../middleware/auth')
+
+// @desc    Show All Pertanyaan
+// @route   GET /pertanyaan
+router.get('/', ensureAuth, (req, res) => {
+  let navMenus = [
+    { link: '/', icon: 'fas fa-chevron-circle-left', label: 'Kembali' }
+  ]
+  res.render('qbank/index', { navTitle:'List Pertanyaan', navMenus })
+})
+
+module.exports = router
