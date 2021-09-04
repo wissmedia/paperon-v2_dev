12f43ebd1c1a33126e242d4870e80d2ffd7ce369
wissmedia <wissmedia@gmail.com>
2021-09-04 15:20:39 +0800

    Create info.js

---
+++ routes/info.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+const { ensureAuth } = require('../middleware/auth')
+
+// @desc    Info Page
+// @route   GET /info
+
+router.get('/', ensureAuth, (req, res) => {
+  let navMenus = [
+    { link: '/', icon: 'fas fa-chevron-circle-left', label: 'Kembali' }
+  ]
+  res.render('info/index', {navTitle:'Info',navMenus})
+})
+
+
+// @desc    Change Log Page
+// @route   GET /info/log
+
+router.get('/log', ensureAuth, (req, res) => {
+  res.render('info/change-log',{navTitle:'Log',})
+})
+
+module.exports = router
