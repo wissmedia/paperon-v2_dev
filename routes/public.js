1eaa3a9bfaa8ec653c56cdc323384776257d07ab
wissmedia <wissmedia@gmail.com>
2021-09-07 22:35:27 +0800

    Update public.js
    update public route

--- routes/public.js
+++ routes/public.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const { ensureAuth } = require('../middleware/auth')
+const Qform = require('../models/qform')
 
 // @desc    Published Kuesioner Page
 // @route   GET /public
-router.get('/', ensureAuth, (req, res) => {
-  let navMenus = []
-  res.render('public/index', { navTitle: 'Kuesioner Publik', navMenus })
+router.get('/', ensureAuth, async (req, res) => {
+  let navMenus = [
+    { link: '/', icon: 'fas fa-chevron-circle-left', label: 'Kembali' },
+  ]
+  try {
+    const qforms = await Qform.find({ status: 'public' })
+      .populate('user')
+      .sort({ createdAt: 'desc' })
+      .lean()
+    res.render('public/index', { qforms, navTitle: 'Kuesioner Publik', navMenus })
+  } catch (error) {
+    console.error(error)
+    return res.render('error/500')
+  }
 })
 
 module.exports = router
