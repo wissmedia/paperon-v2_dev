7d4c25cd42ca7ef330fc56d1adee9d326c965155
wissmedia <wissmedia@gmail.com>
2021-09-12 23:31:27 +0800

    Update noschema.js

--- routes/noschema.js
+++ routes/noschema.js
@@ -7,24 +7,48 @@ const Question = require('../models/question')
 // @desc    NoSchema Page
 // @route   GET /noschema
 router.get('/', ensureAuth, async (req, res) => {
-  const noschemas = await NOX.find().lean()
-  res.render('noschema/index', { noschemas })
+  try {
+    const noschemas = await NOX.find().lean()
+  // res.render('noschema/index', { noschemas })
+  res.type('json').send(JSON.stringify(noschemas, null, 2) + '\n')
+  } catch (error) {
+    console.error(error)
+    return res.render('error/500')
+  }
 })
 
 // @desc    NoSchema Add Page
 // @route   GET /noschema/add
 router.get('/add', ensureAuth, async (req, res) => {
-  const questions = await Question.find({ user: req.user.id }).lean()
-  res.render('noschema/add', { questions })
+  let navMenus = [
+    { link: '/', icon: 'fas fa-chevron-circle-left', label: 'Kembali' },
+  ]
+  try {
+    const questions = await Question.find({ user: req.user.id }).lean()
+    res.render('noschema/add', { questions, navTitle: 'NoSchema Page', navMenus })
+  } catch (error) {
+    console.error(error)
+    return res.render('error/500')
+  }
 })
 
 // @desc    Handle POST NoSchema
 // @route   POST /noschema
 router.post('/', ensureAuth, async (req, res) => {
   try {
-    req.body.user = req.user.id
-    const noschema = new NOX(req.body)
-    await noschema.save()
+    const { bodyText, answerText } = req.body
+    let objs = []
+    for (let i = 0; i < bodyText.length; i++) {
+      obj = {
+        bodyText: bodyText[i],
+        answerText: answerText[i]
+      }
+      objs.push(obj)
+    }
+    await new NOX({
+      user: req.user.id,
+      forms: objs
+    }).save()
     res.redirect('/noschema')
   } catch (error) {
     console.error(error)
