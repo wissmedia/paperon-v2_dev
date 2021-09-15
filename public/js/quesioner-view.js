4cb8edeb2204fa6395aca4042411a89b1c0fed32
wissmedia <wissmedia@gmail.com>
2021-09-15 23:03:38 +0800

    Create quesioner-view.js

---
+++ public/js/quesioner-view.js
@@ -0,0 +1,44 @@
+// Tambahkan Lainnya Radio
+$(document).ready(function(){
+    $("body").on("click",".addLain-radio",function(){ 
+        $(this).parents(".addInput").before(`
+            <p>
+                <div class="setInput">
+                    <input type="radio" name="pilih">
+                    <label>
+                        Lainnya :
+                    </label>
+                    <input type="text" id="input_other" name="opsi">
+                    <button type="button" class="del btn-delInput"><i class="fas fa-times"></i></button>
+                </div>
+            </p>
+        `);
+    });
+});
+
+// Tambahkan Lainnya Checkbox
+$(document).ready(function(){
+    $("body").on("click",".addLain-checkbox",function(){ 
+        $(this).parents(".addInput").before(`
+            <p>
+                <div class="setInput">
+                    <input type="checkbox" name="pilih">
+                    <label>
+                        Lainnya :
+                    </label>
+                    <input type="text" id="input_other" name="opsi">
+                    <button type="button" class="del btn-delInput"><i class="fas fa-times"></i></button>
+                </div>
+            </p>
+        `);
+    });
+});
+
+// Delete Text Input
+$(document).ready(function(){
+    $("body").on("click",".del",function(){
+        $(this).parents(".setInput").fadeOut(function(){
+            $(this).remove();
+        });
+    });
+});
