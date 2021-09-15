f96aaffc029cd2f20490a9b7db8b897337dfceef
wissmedia <wissmedia@gmail.com>
2021-09-16 01:28:32 +0800

    Update viewer.js
    try viewer POST

--- routes/viewer.js
+++ routes/viewer.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth } = require('../middleware/auth')
 const Question = require('../models/question')
+const NOX = require('../models/noSchema')
 
 // @desc    Viewer Page
 // @route   GET /viewer
@@ -11,7 +12,43 @@ router.get('/', ensureAuth, async (req, res) => {
   ]
   try {
     const questions = await Question.find({ user: req.user.id }).lean()
-    res.render('viewer/index', { questions, navTitle: 'Viewer', navMenus })
+    res.render('viewer/index', {
+      questions,
+      helper: require('../helper/helper'),
+      navTitle: 'Viewer',
+      navMenus
+    })
+  } catch (error) {
+    console.error(error)
+    return res.render('error/500')
+  }
+})
+
+// @desc    Process Viewer Page
+// @route   POST /viewer
+router.post('/', ensureAuth, async (req, res) => {
+  try {
+    const { body, tipe, jawaban } = req.body
+    let objs = []
+    for (let i = 0; i < body.length; i++) {
+      obj = {
+        body: body[i],
+        tipe: tipe[i],
+        jawaban: jawaban[i],
+      }
+      objs.push(obj)
+    }
+    let objx = {
+      user: req.user.id,
+      response: objs
+    }
+    console.log(objx)
+
+    // await new NOX({
+    //   user: req.user.id,
+    //   response: objs
+    // }).save()
+    res.redirect('/viewer')
   } catch (error) {
     console.error(error)
     return res.render('error/500')
