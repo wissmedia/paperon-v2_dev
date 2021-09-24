0a4ae8b32cbc09908946451f92acfe741ffe40c5
wissmedia <wissmedia@gmail.com>
2021-09-25 07:44:33 +0800

    Update badah2.js

--- badah2.js
+++ badah2.js
@@ -29,7 +29,78 @@ let kisi2 = {
 const { body, tipe, idQ, ...jawab } = kisi2
 
 
-console.log(body)
-console.log(tipe)
-console.log(idQ)
-console.log(jawab)
+
+
+let a = {
+  '614d9c4b848ed87db77aef03,kolom 1': 'baris 1',
+  '614d9c4b848ed87db77aef03,kolom 2': 'baris 2',
+  '614d9c4b848ed87db77aef04,kolom 1': 'baris 1',
+  '614d9c4b848ed87db77aef04,kolom 2': 'baris 2'
+}
+
+let b = {
+  '614d9c4b848ed87db77aef03': [
+    { 'kolom 1': 'baris 1' },
+    { 'kolom 2': 'baris 2' }
+  ]
+}
+// 
+// ////////////////////////
+let X = Object.keys(a)
+let Y = Object.values(a)
+let Z = []
+let objs = []
+
+for(let i = 0; i< X.length; i++){
+  Z.push(X[i].split(','))
+  Z[i].push(Y[i])
+}
+// console.log(Z)
+// console.log('')
+for(let i = 0; i< Z.length;i++){
+  let obj = {
+      induk: Z[i][0],
+      tanya: Z[i][1],
+      jawab: Z[i][2],
+  }
+  objs.push(obj)
+}
+
+// console.log(objs)
+
+//////////////////////////
+
+let q = {
+  '614d9c4b848ed87db77aef03,kolom 1': 'baris 1',
+  '614d9c4b848ed87db77aef03,kolom 2': 'baris 2',
+  body: [ 'kisi pilihan ganda', 'kisi2' ],
+  tipe: [ 'radioGrid', 'radioGrid' ],
+  idQ: [ '614d9c4b848ed87db77aef03', '614da2f9ec23e68501cfcaab' ],
+  '614da2f9ec23e68501cfcaab,a': '1',
+  '614da2f9ec23e68501cfcaab,b': '3'
+}
+
+
+
+const { body, tipe, idQ, ...jawab } = q
+let X = Object.keys(jawab)
+let Y = Object.values(jawab)
+let Z = []
+let objs = []
+
+console.log(q)
+
+// for(let i = 0; i< X.length; i++){
+//   Z.push(X[i].split(','))
+//   Z[i].push(Y[i])
+// }
+// // console.log(Z)
+// // console.log('')
+// for(let i = 0; i< Z.length;i++){
+//   let obj = {
+//       induk: Z[i][0],
+//       tanya: Z[i][1],
+//       jawab: Z[i][2],
+//   }
+//   objs.push(obj)
+// }
