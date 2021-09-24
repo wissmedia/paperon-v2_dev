8e1ed5721976eb018750ea036328717f14eb7b09
wissmedia <wissmedia@gmail.com>
2021-09-25 07:44:36 +0800

    Create badah3.js

---
+++ badah3.js
@@ -0,0 +1,24 @@
+let a = [
+  {
+    induk: '614d9c4b848ed87db77aef03',
+    tanya: 'kolom 1',
+    jawab: 'baris 1'
+  },
+  {
+    induk: '614d9c4b848ed87db77aef03',
+    tanya: 'kolom 2',
+    jawab: 'baris 2'
+  },
+  {
+    induk: '614d9c4b848ed87db77aef04',
+    tanya: 'kolom 1',
+    jawab: 'baris 1'
+  },
+  {
+    induk: '614d9c4b848ed87db77aef04',
+    tanya: 'kolom 2',
+    jawab: 'baris 2'
+  }
+]
+
+console.log(a[0].induk)
