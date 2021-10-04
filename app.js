26d1ae17246072064e6517c586959cd706a64e7a
wissmedia <wissmedia@gmail.com>
2021-10-05 07:20:06 +0800

    update Change log

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const HOST = process.env.HOST || '0.0.0.0'
 app.locals.appNames = {
   title: 'Paperon',
   subtitle: 'Survei dan Kuesioner',
-  version: '1.1.15'
+  version: '1.1.18'
 }
 
 // BODY PARSER
